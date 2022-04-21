91e83b0068550577abf0324801d4f93f604026c7
Ian Grant <ian@iangrant.me>
2022-04-21 14:15:44 +0100

    Initial commit of main code

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/imgrant/cf-ddns-updater
+
+go 1.17
+
+require (
+	github.com/bobesa/go-domain-util v0.0.0-20190911083921-4033b5f7dd89
+	github.com/cloudflare/cloudflare-go v0.37.0
+	github.com/rdegges/go-ipify v0.0.0-20150526035502-2d94a6a86c40
+)
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/bobesa/go-domain-util/domainutil"
+	"github.com/cloudflare/cloudflare-go"
+	"github.com/rdegges/go-ipify"
+)
+
+type Config struct {
+	FQDN     string
+	APIToken string
+}
+
+var config Config
+
+func updateDNSRecord(fqdn string, recordType string, ip string, checkMode bool) error {
+	api, err := cloudflare.NewWithAPIToken(config.APIToken)
+	if err != nil {
+		return err
+	}
+
+	domain := domainutil.Domain(fqdn)
+	zoneID, err := api.ZoneIDByName(domain)
+	if err != nil {
+		return err
+	}
+
+	fqdnRecord := cloudflare.DNSRecord{Name: fqdn}
+	records, err := api.DNSRecords(context.Background(), zoneID, fqdnRecord)
+	if err != nil {
+		return err
+	}
+
+	for _, r := range records {
+		if r.Content == ip {
+			log.Printf("%s points to current IP address, no change is needed.", r.Name)
+			continue
+		}
+		log.Printf("%s points to %s, the record will be updated.", r.Name, r.Content)
+
+		if checkMode == true {
+			log.Println("Check mode is active, no changes will be made.")
+		} else {
+			log.Println("Setting", fqdn, "=>", ip, "...")
+			r.Content = ip
+			err := api.UpdateDNSRecord(context.Background(), zoneID, r.ID, r)
+			if err != nil {
+				return err
+			}
+			log.Println("Success!")
+		}
+
+	}
+	return nil
+}
+
+func loadConfig(fn string) error {
+	f, err := os.Open(fn)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewDecoder(f).Decode(&config)
+}
+
+func main() {
+	cf := flag.String("c", "config.json", "Config file")
+	check_mode := flag.Bool("n", false, "Check mode (dry run)")
+	flag.Parse()
+	err := loadConfig(*cf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ipv4, err := ipify.GetIp()
+	if err != nil {
+		log.Fatal("Couldn’t determine the current IP address:", err)
+	} else {
+		log.Println("Current IP address is:", ipv4)
+	}
+
+	err = updateDNSRecord(config.FQDN, "A", ipv4, *check_mode)
+	if err != nil {
+		log.Println(err)
+	}
+}
